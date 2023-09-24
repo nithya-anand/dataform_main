0478315b619bbc4435a10daee3c8f1b7af2b79d6
Nithya Anand <nithyelectrok@gmail.com>
2023-09-24 16:17:35 +0000

    24sepcommit

--- includes/constants.js
+++ includes/constants.js
@@ -1,7 +1,6 @@
   // filename is includes/constants.js
   const PROJECT_ID = "my-clo-proj-nit";
   const SCHEMA_NAME = "patient";
-  const RUN_ID="concat(current_datetime(),'cerner')"
-  const STEP_ID="concat(current_datetime(),'cerner_step1')"
-  module.exports = { PROJECT_ID,SCHEMA_NAME,RUN_ID,STEP_ID };
+  const RUN_ID="concat(FORMAT_DATE('%d%m%G%I%M%S',current_datetime),'cerner-pat')";
+  module.exports = { PROJECT_ID,SCHEMA_NAME,RUN_ID};
   
